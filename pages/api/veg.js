49c1f2d4874474d77e9b2f78ee1c879158c503da
LauraCullenHill <laurahill207@hotmail.com>
2023-02-09 11:23:42 +0000

    SQL added

--- pages/api/veg.js
+++ pages/api/veg.js
@@ -1,24 +1,10 @@
-export default function handler(req, res) {
-    const vegs = [
-        {
-            id: "1",
-            type: "The Hobbit",
-            variety: "J.R.R. Tolkien",
-            imgUrl: "/assets/1.png"
-        },
-        {
-            id: "2",
-            type: "The Fellowship of the Ring",
-            variety: "J.R.R. Tolkien",
-            imgUrl: "/assets/2.png"
-        },
-        {
-            id: "3",
-            type: "Born a Crime",
-            variety: "Trevor Noah",
-            imgUrl: "/assets/3.png"
-        }
-    ];
+import sql from "/.env"
+
+export default async function handler(req, res) {
+
+    const vegs = await sql
+        select * from vegs
+    '
 
     res.json(vegs);
 }
