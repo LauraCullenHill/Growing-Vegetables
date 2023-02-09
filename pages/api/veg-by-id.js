711f15a2eacc43e393f17e90483a20c9a19ea81b
LauraCullenHill <laurahill207@hotmail.com>
2023-02-09 11:34:36 +0000

    single veg pulled from database

--- pages/api/veg-by-id.js
+++ pages/api/veg-by-id.js
@@ -1,35 +1,50 @@
-export default function handler(req, res) {
+import sql from "@/components/utils/postgres";
+
+export default async function handler(req, res) {
     const id = req.query.id;
 
-    const vegs = [
-        {
-            id: "1",
-            type: "Tomatoe",
-            variety: "San Marco",
-            imgUrl: "/assets/1.png"
-        },
-        {
-            id: "2",
-            type: "Cucumber",
-            variety: "",
-            imgUrl: "/assets/2.png"
-        },
-        {
-            id: "3",
-            type: "Spinach",
-            variety: "",
-            imgUrl: "/assets/3.png"
-        }
-    ];
+    const vegs = sql `
+        select = from vegs
+        where id = ${id}
+    `;
 
-    const foundVeg = vegs.find((veg) => {
-        if (veg.id === id) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    });
+    if (veg.length < 1) {} 
+    res.status{404}.json({ message: "Vegetable not found"});
+    return
+    
+    const foundVeg = vegs[0];
 
     res.json({ veg: foundVeg });
 }
+
+
+    // const vegs = [
+    //     {
+    //         id: "1",
+    //         type: "Tomatoe",
+    //         variety: "San Marco",
+    //         imgUrl: "/assets/1.png"
+    //     },
+    //     {
+    //         id: "2",
+    //         type: "Cucumber",
+    //         variety: "",
+    //         imgUrl: "/assets/2.png"
+    //     },
+    //     {
+    //         id: "3",
+    //         type: "Spinach",
+    //         variety: "",
+    //         imgUrl: "/assets/3.png"
+    //     }
+    // ];
+
+    // const foundVeg = vegs.find((veg) => {
+    //     if (veg.id === id) {
+    //         return true;
+    //     }
+    //     else {
+    //         return false;
+    //     }
+    // });
+
--- pages/api/veg.js
+++ pages/api/veg.js
@@ -2,7 +2,7 @@ import sql from "/.env"
 
 export default async function handler(req, res) {
 
-    const vegs = await sql
+    const vegs = await sql '
         select * from vegs
     '
 
