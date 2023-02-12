d51c9132654cf7bbe3112d4e3a37e36cc821dfe3
LauraCullenHill <laurahill207@hotmail.com>
2023-02-12 15:47:43 +0000

    fixed issues

--- components/home/hero/index.js
+++ components/home/hero/index.js
@@ -7,7 +7,7 @@ export default function Hero({ imgUrl, title, subtitle }) {
                 </div>
                 <div className="w-10/12">
                     <h1 className="text-4xl font-bold">{title}</h1>
-                    <p className="text-xl text-gray-700">{subtitle}</p>
+                    <p className="text-xl text-black">{subtitle}</p>
                 </div>
             </div>
         </div>
--- components/home/topics/topics.js
+++ components/home/topics/topics.js
@@ -1,10 +1,6 @@
 export default [
-    "React",
-    "State hook",
-    "Effect hook",
-    "Next.js",
-    "Node.js",
-    "Git",
-    "React components",
-    "Mobile responsive design"
+    "Species",
+    "Variety",
+    "Seasons to plant and harvest",
+    
 ];
