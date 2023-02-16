b1196adf6c83f404f3f27c2382a9a312bd60c304
LauraCullenHill <laurahill207@hotmail.com>
2023-02-16 16:45:19 +0000

    x

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,29 @@
-
-import Hero from "/components/home/hero";
-import Topics from "/components/home/topics";
-import Content from "/components/shared/content";
-import Footer from "/components/shared/footer";
-import Header from "/components/shared/header";
+import Hero from "@/components/home/hero";
+import Topics from "@/components/home/topics";
+import Content from "@/components/shared/content";
+import Footer from "@/components/shared/footer";
+import Header from "@/components/shared/header";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
+
 
 
 export default function Home() {
   const router = useRouter();
   const [state, setState] = useState({});
 
-  const handleClick = () => {
-    alert("you clicked the button!");
 
-    // make API call
-
-    router.push("/vegs");
-    // navigate to the vegs page
+    router.push("/vegetables");
+    // navigate to the vegetabless page
   }
 
   return (
     <div className="w-full">
-      <Header name="Home" />
+      <Header name="Grow Your Own" />
 
       <Hero
         imgUrl="/public/assets/garden.jpg"
-        title="Grow Your Own"
         subtitle="A simple guide to British growing"
       />
 
