0b2a8ac22ba4836d11435860da81522a6f21ba05
LauraCullenHill <laurahill207@hotmail.com>
2023-02-16 17:28:22 +0000

    xx

--- pages/api/vegs.js
+++ pages/api/vegs.js
@@ -2,9 +2,9 @@ import sql from "/components/utils/postgres"
 
 export default async function handler(req, res) {
 
-    const vegs = await sql `
-        select * from vegs
+    const veg = await sql `
+        select * from veg
     `;
 
-    res.json(vegs);
+    res.json(veg);
 }
