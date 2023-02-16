a8dbd6696cc27b3330238363e08443d606c8591e
LauraCullenHill <laurahill207@hotmail.com>
2023-02-16 17:45:26 +0000

    xx

--- components/vegs/veg-grid/veg-item.js
+++ components/vegs/veg-grid/veg-item.js
@@ -1,26 +1,27 @@
 import Link from "next/link";
 import { useState } from "react";
 
-export default function VegsItem({ veg }) {
+export default function VegItem({ veg }) {
     const [likes, setLikes] = useState(0);
 
     const handleLike = async () => {
         let newLikes = likes + 1;
-        const id = veg.id;
+        setLikes(newLikes);
+
+        const id = +veg.id;
         const response = await fetch(`/api/increase-likes?id=${id}&likes=${newLikes}`);
         const data = await response.json();
-        setLikes(newLikes);
     }
 
     return (
         <div className="border rounded-lg p-4">
-            <img src={veg.imgUrl} className="rounded-lg" />
-            <p className="text-xl font-medium">{veg.type}</p>
-            <p className="text-gray-700">{veg.author}</p>
+            <img src={vegs.imgUrl} className="rounded-lg" />
+            <p className="text-xl font-medium">{vegs.type}</p>
+            <p className="text-gray-700">{vegs.variety}</p>
             <button
                 onClick={handleLike}
                 type="button"
-                className="bg-blue-500 px-2 py-1 rounded text-white"
+                className="bg-green-500 px-2 py-1 rounded text-white"
             >
                 Like ({likes} likes)
             </button>
