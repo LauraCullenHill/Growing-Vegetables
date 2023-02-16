d5d9f4455448e976ff361ad1dcc6934ed0f8fd41
LauraCullenHill <laurahill207@hotmail.com>
2023-02-16 16:56:48 +0000

    x

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
 
   return (
     <div className="w-full">
-      <Header name="Home" />
+      <Header name="Grow Your Own" />
 
       <Hero
         imgUrl="/assets/garden.jpg"
