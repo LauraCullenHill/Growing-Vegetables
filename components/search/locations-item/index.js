754e533a3ad8fcfa84b52b57f3f65e380cf3fb81
LauraCullenHill <laurahill207@hotmail.com>
2023-02-16 12:36:34 +0000

    search update

---
+++ components/search/locations-item/index.js
@@ -0,0 +1,13 @@
+export default function (LocationItem{ location}) {
+   const { id, title, img_ur, visited_at } = location;
+   
+   
+    return (
+        <div key={location.id} className="w-full h-40 bg-red-100 relative">
+            <img src={img_url} className="wifull h-full brightness-75" />
+            <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
+                <p className="text-white font-bold size-3xl">{title}</p>
+            </div>
+        </div>
+    )                  
+}
