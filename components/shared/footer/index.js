477a4fa15a37a2fbe00f6741f11bcc7514e08f14
LauraCullenHill <laurahill207@hotmail.com>
2023-02-16 15:45:01 +0000

    x

--- components/shared/footer/index.js
+++ components/shared/footer/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer({ href, title }) {
     return (
-        <div className="w-full max-w-6xl mx-auto mt-8">
+        <div className="w-full max-w-6xl mx-auto mt-8 font-manrope">
             <Link href={href}>
                 Go to {title}
             </Link>
--- components/shared/header/index.js
+++ components/shared/header/index.js
@@ -1,7 +1,7 @@
 export default function Header({ name }) {
     return (
-        <div className="bg-white-100 w-full max-w-6xl mx-auto py-4 px-4">
-            <h1 className="font-bold text-4xl">{name} Page</h1>
+        <div className="bg-white-100 w-full max-w-6xl mx-auto py-4 px-4 font-manrope">
+            <h1 className="font-bold text-4xl font-manrope">{name} Page</h1>
         </div>
     );
 }
